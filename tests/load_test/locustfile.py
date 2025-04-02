8d91f7c9dcc6be8415699ab4c66c013f7497eefa
grefsguide <grshaulov@gmail.com>
2025-04-02 17:58:07 +0500

    Add files via upload

--- tests/load_test/locustfile.py
+++ tests/load_test/locustfile.py
@@ -1,8 +1,13 @@
 from locust import HttpUser, task, between
-import uuid
+import secrets
 
 class ShortenerUser(HttpUser):
     wait_time = between(1, 3)
+    short_code = None
+
+    def gen_short_code(self, length: int = 10) -> str:
+        alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return ''.join(secrets.choice(alphabet) for _ in range(length))
 
     @task
     def log(self):
@@ -11,15 +16,17 @@ class ShortenerUser(HttpUser):
         )
     @task
     def create_link(self):
+        self.short_code = self.gen_short_code()
         self.client.post(
             "/api/links/shorten",
             json={
             "original_url": "https://example.com/test",
-            "custom_alias": f"{uuid.uuid4()}"
+            "custom_alias": f"{self.short_code}"
                  }
         )
 
     @task(3)
     def redirect_link(self):
-        self.client.get(f"/api/links/{uuid.uuid4()}")
+        if self.short_code:
+            self.client.get(f"/api/links/{self.short_code}")
 
