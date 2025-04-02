ef776f24f10f7a0357d418343dafad97e247e814
grefsguide <grshaulov@gmail.com>
2025-04-02 18:25:28 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -13,6 +13,6 @@ REDIS_URL = os.getenv("REDIS_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 TTL = 30
-TTL_LINK = 1
+TTL_LINK = 1 #Дни жизни ссылки, сначала переходит в состояние неактивности и через то же время удаляется(если не обновить)
 CLEANUP_INTERVAL = 86400 #sec
 DATABASE_URL = os.getenv("DATABASE_URL", f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
